be769d18175ea2ee3eef79ce20e569792204f7d1
Ankush Singh <ankushsahil45@gmail.com>
2022-08-18 00:56:29 +0530

    added functions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Left from "./components/Left/Left";
+import Middle from "./components/Middle/Middle";
+import Right from "./components/Right/Right";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [type, setType] = useState("");
+  const [wishlist, setWishList] = useState(
+    JSON.parse(localStorage.getItem("wishlist")) || []
   );
+  const [searchText, setSearchText] = useState("");
+
+  // ===============Save to localStorage====================
+  const saveToLocalStorage = (items) => {
+    localStorage.setItem("wishlist", JSON.stringify(items));
+  };
+
+  // ===================Set as favorite=======================
+  function setFav({ mov, type }) {
+    console.log(mov, type);
+    const alreadyWishListed = wishlist.find((wish) => wish.mov.id === mov.id);
+    if (alreadyWishListed) return;
+    const temp = [{ mov, type }, ...wishlist];
+    setWishList(temp);
+    saveToLocalStorage(temp);
+    setType(temp);
+  }
+  // ======================Remove from favorite====================
+  const removeWishHandler = (id) => {
+    const temp = wishlist.filter((tmp) => tmp.mov.id !== id);
+    setWishList(temp);
+    saveToLocalStorage(temp);
+  };
+  // =====================Pass search value to middle component from right========================
+  function getSearchHandler(text) {
+    setSearchText(text);
+  }
+  return <div className="App"></div>;
 }
 
 export default App;
