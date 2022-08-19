44f634d0ccffad522c24e764fc49ba2eebf848db
Ankush Singh <ankushsahil45@gmail.com>
2022-08-19 23:17:05 +0530

    added Right section

--- src/components/Right/Right.js
+++ src/components/Right/Right.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Favorites from "./Favorites";
+import InputArea from "./InputArea";
 
-const Right = () => {
-  return <div></div>;
+const Right = ({ wishlist, removeWish, type, getSearch }) => {
+  return (
+    <div className="right">
+      <InputArea getSearch={getSearch} />
+      <Favorites wishlist={wishlist} removeWish={removeWish} />
+    </div>
+  );
 };
 
 export default Right;
