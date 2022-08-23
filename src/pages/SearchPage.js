8b0b779a145cf6cecf99016d0761399ec3e36474
Ankush Singh <ankushsahil45@gmail.com>
2022-08-23 19:54:10 +0530

    added searchPage

---
+++ src/pages/SearchPage.js
@@ -0,0 +1,110 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { GoAlert } from "react-icons/go";
+import uuid from "react-uuid";
+import API_KEY from "../components/Axios/Requests";
+import Card from "../components/Middle/Card";
+import Spinner from "../components/Spinner/Spinner";
+import Axios from "../components/Axios/Axios";
+
+const SearchPage = ({ searchText, setFav, wishlist }) => {
+  const [content, setContent] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [totalPage, setTotalPage] = useState(0);
+  const params = useParams();
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        setLoading(true);
+        const data = await Axios.get(
+          `https://api.themoviedb.org/3/search/${params.type}?api_key=${API_KEY}&language=en-US&query=${searchText}&page=${page}&include_adult=false`
+        );
+
+        setContent(data.results);
+        setLoading(false);
+        setTotalPage(data.total_pages);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    getData();
+  }, [searchText, params.type, page]);
+
+  useEffect(() => {
+    setPage(1);
+  }, [searchText, params.type]);
+
+  return (
+    <div className="searchPage">
+      <div className="header">
+        <div>
+          <NavLink
+            to="/searchPage/movie"
+            className={(navdata) => (navdata.isActive ? "active" : "link")}
+          >
+            Movies
+          </NavLink>
+        </div>
+
+        <div>
+          <NavLink
+            to="/searchPage/tv"
+            className={(navdata) => (navdata.isActive ? "active" : "link")}
+          >
+            TV Series
+          </NavLink>
+        </div>
+      </div>
+      <div>
+        {loading ? (
+          <Spinner />
+        ) : (
+          <div className="cardHolder">
+            {content.length > 0 ? (
+              content.map((dt) => (
+                <div className="inn" key={uuid()}>
+                  <Card
+                    mov={dt}
+                    key={uuid()}
+                    setFav={setFav}
+                    type={params.type}
+                    wishlist={wishlist}
+                  />
+                </div>
+              ))
+            ) : (
+              <div className="notFound">
+                <div className="alert">
+                  <GoAlert />
+                </div>
+                <div>Not Found</div>
+              </div>
+            )}
+          </div>
+        )}
+        <div className="loadMore">
+          <div>
+            {page > 1 && (
+              <div className="btn" onClick={() => setPage(page - 1)}>
+                <div className="btnIn">Prev</div>
+              </div>
+            )}
+          </div>
+          <div>
+            {totalPage > 1 && page <= totalPage && (
+              <div className="btn" onClick={() => setPage(page + 1)}>
+                <div className="btnIn">Next</div>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchPage;
