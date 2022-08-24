ac83d0adc6951a91d700dab75b080ee7ea87d7cc
Ankush Singh <ankushsahil45@gmail.com>
2022-08-24 22:17:20 +0530

    added Middle section

--- src/components/Middle/Middle.js
+++ src/components/Middle/Middle.js
@@ -1,7 +1,198 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import uuid from "react-uuid";
+import { NavLink } from "react-router-dom";
+import { ImCross } from "react-icons/im";
+import { GiHamburgerMenu } from "react-icons/gi";
+import Axios from "../Axios/Axios";
+import Requests from "../Axios/Requests";
+import HomePage from "../../pages/HomePage";
+import CategoryPage from "../../pages/CategoryPage";
+import DetailPage from "../../pages/DetailPage";
+import SearchPage from "../../pages/SearchPage";
+import "./Middle.css";
 
-const Middle = () => {
-  return <div></div>;
+const Middle = ({ setFav, searchText, wishlist }) => {
+  const [trendingMovie, setTrendingMovie] = useState([]);
+  const [sideNav, setSideNav] = useState(false);
+  const [topMovie, setTopMovie] = useState([]);
+  const [trendingShow, setTrendingShow] = useState([]);
+  const [topShow, setTopShow] = useState([]);
+  const [bannerMovie, setBannerMovie] = useState([]);
+  const [bannerShow, setBannerShow] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const catArr = [
+    "Action",
+    "Comedy",
+    "Drama",
+    "Family",
+    "War",
+    "Documentary",
+    "Crime",
+    "Mystery",
+    "Animation",
+    "Western",
+  ];
+
+  async function temp() {
+    async function getTrendingMovie() {
+      setLoading(true);
+      const res = await Axios.get(Requests.fetchTrendingMovie);
+      setTrendingMovie(res.data.results);
+      setLoading(false);
+      return res.data.results;
+    }
+    getTrendingMovie();
+
+    async function getTopMovie() {
+      setLoading(true);
+      const res = await Axios.get(Requests.fetchTopRatedMovie);
+      setTopMovie(res.data.results);
+      setLoading(false);
+    }
+    getTopMovie();
+
+    async function getTrendingShow() {
+      setLoading(true);
+      const res = await Axios.get(Requests.fetchTrendingShow);
+      setTrendingShow(res.data.results);
+      setLoading(false);
+      return res.data.results;
+    }
+    getTrendingShow();
+
+    async function getTopShow() {
+      setLoading(true);
+      const res = await Axios.get(Requests.fetchTopRatedShow);
+      setTopShow(res.data.results);
+      setLoading(false);
+    }
+    getTopShow();
+
+    const movieForBanner = await getTrendingMovie();
+    setBannerMovie(
+      movieForBanner[Math.floor(Math.random() * movieForBanner.length - 1)]
+    );
+
+    const showForBanner = await getTrendingMovie();
+    setBannerShow(
+      showForBanner[Math.floor(Math.random() * showForBanner.length - 1)]
+    );
+  }
+
+  useEffect(() => {
+    temp();
+  }, []);
+
+  // =====================Open and close side nav================
+  function openSideNav() {
+    setSideNav(!sideNav);
+  }
+
+  function closeSideNav() {
+    setSideNav(false);
+  }
+
+  return (
+    <div className="middle">
+      {!sideNav ? (
+        <div className="hamburger" onClick={openSideNav}>
+          <GiHamburgerMenu />
+        </div>
+      ) : (
+        <div className="close" onClick={closeSideNav}>
+          <ImCross />
+        </div>
+      )}
+
+      {sideNav && (
+        <div className="res">
+          <div>
+            <div className="menuR">
+              <div>
+                <NavLink
+                  to="/"
+                  className={(navdata) =>
+                    navdata.isActive ? "activeL" : "link"
+                  }
+                  onClick={closeSideNav}
+                >
+                  Home
+                </NavLink>
+              </div>
+              {catArr.map((cat) => (
+                <div key={uuid()}>
+                  <NavLink
+                    to={`/${cat}/movie`}
+                    className={(navdata) =>
+                      navdata.isActive ? "activeL" : "link"
+                    }
+                    onClick={closeSideNav}
+                  >
+                    {cat}
+                  </NavLink>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      )}
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <HomePage
+              trending={trendingMovie}
+              top={topMovie}
+              banner={bannerMovie}
+              setFav={setFav}
+              type="movie"
+              loading={loading}
+              wishlist={wishlist}
+            />
+          }
+        />
+
+        <Route
+          path="/tv"
+          element={
+            <HomePage
+              trending={trendingShow}
+              top={topShow}
+              banner={bannerShow}
+              setFav={setFav}
+              type="tv"
+              loading={loading}
+              wishlist={wishlist}
+            />
+          }
+        />
+
+        <Route
+          path="/detailPage/:id/:type"
+          element={<DetailPage setFav={setFav} />}
+        />
+
+        <Route
+          path="/:category/:type"
+          element={<CategoryPage setFav={setFav} wishlist={wishlist} />}
+        />
+
+        <Route
+          path="/searchPage/:type"
+          element={
+            <SearchPage
+              searchText={searchText}
+              setFav={setFav}
+              wishlist={wishlist}
+            />
+          }
+        />
+      </Routes>
+    </div>
+  );
 };
 
 export default Middle;
